3cf9f30b47ee974909d894883ce8f763a91c1a54
JT Raber <JMANN240raber@gmail.com>
2026-02-11 11:48:43 -0500

    logging

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
-use std::path::Path;
+use std::{path::Path, sync::{Arc, Mutex}};
 
 use chrono::Duration;
 use hound::{WavSpec, WavWriter};
 
 use rayon::prelude::*;
+use tracing::debug;
 
 use crate::document::Document;
 
@@ -23,6 +24,7 @@ pub fn render<P: AsRef<Path>>(
     duration: Duration,
     document: Document,
 ) -> Result<(), hound::Error> {
+    debug!("creating spec");
     let spec = WavSpec {
         channels: 1,
         sample_rate: 44100,
@@ -30,26 +32,40 @@ pub fn render<P: AsRef<Path>>(
         sample_format: hound::SampleFormat::Float,
     };
 
+    debug!("calculating total samples");
     let total_samples = (spec.channels as f64 * duration.as_seconds_f64() * spec.sample_rate as f64)
         .ceil() as usize;
 
+    debug!("allocating samples vector");
     let mut mix = vec![0.0f32; total_samples];
 
+    let samples_completed = Arc::new(Mutex::new(0));
+
+    debug!("rendering");
     (0..total_samples)
         .into_par_iter()
         .map(|i| {
             let t = i as f64 / spec.sample_rate as f64;
 
-            document.eval(t) as f32
+            let value = document.eval(t) as f32;
+
+            let mut lock = samples_completed.lock().unwrap();
+            *lock += 1;
+            debug!("{}/{}", *lock, total_samples);
+
+            value
         })
         .collect_into_vec(&mut mix);
 
+    debug!("creating writer");
     let mut writer = WavWriter::create(filename, spec)?;
 
+    debug!("writing samples");
     for sample in mix {
         writer.write_sample(sample)?;
     }
 
+    debug!("finalizing writer");
     writer.finalize()?;
 
     Ok(())
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use std::path::PathBuf;
 use chrono::Duration;
 use musath::{MusathParser, Rule, document::Document, header::HeaderValue, render};
 use pest::Parser;
+use tracing::info;
+use tracing_subscriber::EnvFilter;
 
 #[derive(clap::Parser)]
 struct Args {
@@ -10,11 +12,24 @@ struct Args {
 }
 
 fn main() {
+    let subscriber = tracing_subscriber::fmt()
+        .compact()
+        .with_file(true)
+        .with_line_number(true)
+        .with_thread_ids(true)
+        .with_target(false)
+        .with_env_filter(EnvFilter::from_default_env())
+        .finish();
+
+    tracing::subscriber::set_global_default(subscriber).unwrap();
+
     let args = <Args as clap::Parser>::parse();
 
     let unparsed_file = std::fs::read_to_string(args.path).expect("cannot read file");
 
+    info!("Parsing...");
     let file = Document::parse(&mut MusathParser::parse(Rule::document, &unparsed_file).unwrap());
+    info!("Parsed!");
 
     let output_filename_header_value = file
         .header()
@@ -30,6 +45,7 @@ fn main() {
         .cloned()
         .unwrap_or(HeaderValue::Number(30.0));
 
+    info!("Rendering...");
     if let HeaderValue::String(output_filename) = output_filename_header_value
         && let HeaderValue::Number(duration) = duration_header_value
     {
@@ -40,4 +56,5 @@ fn main() {
         )
         .unwrap();
     }
+    info!("Rendered!");
 }
